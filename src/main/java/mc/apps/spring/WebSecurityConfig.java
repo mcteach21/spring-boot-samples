744aeae9bd4818209dfdc32cdda3c286605d68fe
MC <mc69.dashboard@gmail.com>
2021-06-08 17:05:55 +0200

    spring security config.

---
+++ src/main/java/mc/apps/spring/WebSecurityConfig.java
@@ -0,0 +1,67 @@
+package mc.apps.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**","/webjars/**", "/").permitAll()
+                .antMatchers("/admin/**").hasRole("USER")
+                .and()
+                .formLogin()
+                .loginPage("/login").failureUrl("/login-error");
+
+
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/css/**","/webjars/**", "/").permitAll()
+//                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+//                .antMatchers("/user/**").hasAnyRole("USER")
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll()
+//                .and()
+//                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("123").roles("USER");
+    }
+
+/*
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }*/
+}
--- src/main/java/mc/apps/spring/controllers/MyErrorController.java
+++ src/main/java/mc/apps/spring/controllers/MyErrorController.java
@@ -2,13 +2,15 @@ package mc.apps.spring.controllers;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyErrorController implements ErrorController {
 
-    @RequestMapping("/error")
-    public String handleError() {
+    @RequestMapping({"/error","/login-error"})
+    public String handleError(Model model) {
+        model.addAttribute("error","url non valide!");
         return "error";
     }
 
