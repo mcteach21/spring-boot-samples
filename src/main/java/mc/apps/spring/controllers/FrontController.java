8a28ebccbec2cf4f5d9484baa99055d41e36ba65
MC <mc69.dashboard@gmail.com>
2021-06-18 16:54:29 +0200

    add model

--- src/main/java/mc/apps/spring/controllers/FrontController.java
+++ src/main/java/mc/apps/spring/controllers/FrontController.java
@@ -1,5 +1,6 @@
 package mc.apps.spring.controllers;
 
+import mc.apps.spring.model.User;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,10 +9,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
@@ -19,36 +18,51 @@ import java.security.Principal;
 @Controller
 public class FrontController {
     private static final Logger logger = LogManager.getLogger(FrontController.class);
+    private static final String DEFAULT_PATH = "/";
 
-    @RequestMapping(value="/")
-    public String index(@RequestParam(required=false, defaultValue="index") String page,  Model model,
-                        Authentication authentication){
+//    @RequestMapping(value="/")
+//    public String index(@RequestParam(required=false, defaultValue="index") String page,  Model model, Authentication authentication){
+//
+//        model.addAttribute("title", "Spring Boot");;
+//        model.addAttribute("logged", (authentication==null)?"":authentication.getName());
+//        model.addAttribute("user", new User());
+//        return page;
+//    }
 
+    @RequestMapping(value={"/","/{action}"})
+    public String display(@PathVariable(required = false) String action,  Model model, Authentication authentication){
 
-        model.addAttribute("title", "Spring Boot");
-        String loggedin =  (authentication==null)?"":authentication.getName();
-        model.addAttribute("logged", loggedin);
+        String page = (action==null)?"index":action;
 
-        logger.log(Level.INFO, "page = "+page+" - logged user : "+loggedin);
+        logger.log(Level.INFO, "****************************************");
+        logger.log(Level.INFO, "page = "+page);
+        logger.log(Level.INFO, "****************************************");
+
+        model.addAttribute("title", formatted(page));
+        model.addAttribute("logged", (authentication==null)?"":authentication.getName());
+        model.addAttribute("user", new User());
+
         return page;
     }
 
-    @RequestMapping(value="/{action}")
-    public String display(@PathVariable String action,  Model model, Authentication authentication){
 
+    @PostMapping(value="/{action}")
+    public String post(@PathVariable String action, @ModelAttribute Object object){
         logger.log(Level.INFO, "action = "+action);
-        //        if(action.equals("logout")) {
-        //            logger.info("*********************************************");
-        //            logger.info("Bye!");
-        //            logger.info("*********************************************");
-        //            return "redirect:/";
-        //        }
+        switch (action){
+            case "signup":
+//                User user = (User)object;
+                logger.log(Level.INFO, "User = "+object.getClass().getSimpleName());
+                break;
+            default:
+                break;
+        }
 
-        model.addAttribute("title", formatted(action));
-        model.addAttribute("logged", (authentication==null)?"":authentication.getName());
-        return action;
+        return "redirect:"+DEFAULT_PATH;
     }
     private String formatted(String page) {
         return page.substring(0,1).toUpperCase()+page.substring(1).toLowerCase();
     }
+
+
 }
---
+++ src/main/java/mc/apps/spring/model/User.java
@@ -0,0 +1,68 @@
+package mc.apps.spring.model;
+
+import org.springframework.context.annotation.Bean;
+
+public class User {
+    private int id;
+    private String prenom;
+    private String nom;
+    private String login;
+    private String password;
+
+    public User(int id, String prenom, String nom, String login, String password) {
+        this.id = id;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.login = login;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "[" +  id +  "] " + prenom + " " + nom;
+    }
+
+}
